b881c9592594804c9d22c9a0224b46ea25d3658b
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-07-04 16:54:55 +0200

    bootstrap imported

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php 
  echo "ciao"
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+      <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <h1>
+        ciao mare 
+    </h1>
+</body>
+</html>

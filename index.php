29b9fcc53d7504c51d22fcb27cd06d6bae6e00d3
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-07-05 09:39:27 +0200

    commit I forgot yesterday but it's incomplete

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<?php 
- echo "ciao"
+<?php
+function getRandomPassword($length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = rand(0, strlen($characters) - 1);
+        $password .= $characters[$randomIndex];
+    }
+    return $password;
+}
+
+if (isset($_GET['length'])) {
+    $length = intval($_GET['length']);
+    $password = getRandomPassword($length);
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,8 +26,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <h1>
-        ciao mare 
-    </h1>
+<form method="GET" action="">
+        <label for="length">lunghezza password::</label>
+        <input type="number" name="length" id="length" value="" placeholder="Minimum 4" required min="4">
+        <input type="submit" value="Generate password">
+    </form>
+
+   
 </body>
 </html>
